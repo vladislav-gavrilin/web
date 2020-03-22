1be024c4921540c98e606a0f5e618e394f03ce74
vladislav-gavrilin <vlad-gavrilin@mail.ru>
2020-03-22 15:03:56 +0300

    lab2

---
+++ lab2/print_args/print_args/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("No parameters were specified!");
+			}
+			else
+			{
+				Console.WriteLine("Number of arguments: " + args.Length);
+				Console.Write("Arguments: ");
+				for (int i = 0; i < args.Length; ++i)
+				{
+					Console.Write(args[i] + " ");
+				}
+			}
+		}
+	}
+}
---
+++ lab2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remove_duplicates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length !=1)
+			{
+				Console.WriteLine("Incorrect number of arguments!");
+				Console.WriteLine("Usage remove_duplicates.exe <input string>");
+			}
+			else
+			{
+				Console.WriteLine(args[0].Distinct().ToArray());
+			}
+		}
+	}
+}
